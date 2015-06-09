90892a3b6fecb2783274a278c1b57dd25102c7bd
Jacob Millward <jacob.millward@gmail.com>
2015-06-09 16:43:28 +0100

    Fix indentation a little

--- src/com/jacobmillward/libkettlecontrol/Kettle.java
+++ src/com/jacobmillward/libkettlecontrol/Kettle.java
@@ -18,133 +18,133 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Kettle {
-	
-	private Socket sock;
-	private KettleListener listener;
-	private Thread listenerThread;
-	private OutputStream outputStream;
-	private InputStream inputStream;
-	private String hostname;
-	private final int port = 2000;
-	public String ipPrefix="192.168.1.";
-	
-	public String getHostname() {
-		return hostname;
-	}
+    
+    private Socket sock;
+    private KettleListener listener;
+    private Thread listenerThread;
+    private OutputStream outputStream;
+    private InputStream inputStream;
+    private String hostname;
+    private final int port = 2000;
+    public String ipPrefix="192.168.1.";
+    
+    public String getHostname() {
+        return hostname;
+    }
 
-	public int getPort() {
-		return port;
-	}
+    public int getPort() {
+        return port;
+    }
 
-	public Kettle() {
-		this("192.168.1.");
-	}
-	public Kettle(String networkPrefix) {
-		this.ipPrefix = networkPrefix;
-		sock = scan();
-		if(sock != null) {
-			this.hostname = sock.getInetAddress().getHostAddress();
-			try {
-				inputStream = sock.getInputStream();
-				outputStream = sock.getOutputStream();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-			listener = new KettleListener(inputStream);
-			listenerThread = new Thread(listener);
-			listenerThread.start();
-		}
-		else System.out.println("No kettle found");
-	}
-	
-	public Kettle(InetAddress address){
-		//Store hostname and port
-		this.hostname = address.getHostAddress();
-		
-		try {
-			sock = new Socket(hostname, port); //Attempt to create a socket
-			outputStream = sock.getOutputStream();
-			inputStream = sock.getInputStream();
-		}
-		catch (IOException e) {
-			System.out.println("Could not create socket");
-			e.printStackTrace();
-		}
-		listener = new KettleListener(inputStream);
-		listenerThread = new Thread(listener);
-		listenerThread.start();
-	}
-	
-	public Socket scan() {
-		for (int i = 1; i < 254; i++){
-			System.out.println(ipPrefix+i);
-			try {
-			    Socket mySocket = new Socket();
-			    SocketAddress address = new InetSocketAddress(ipPrefix+i, port);
-			    mySocket.setSoTimeout(2000);
-			    mySocket.connect(address, 500);   
-			    
-			    BufferedReader in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
-			    send(mySocket.getOutputStream(), "HELLOKETTLE\n");
-			    try {
-			        String response = in.readLine();
-			        if (response.startsWith("HELLOAPP")) {
-			            	return mySocket;
-			            }
-			        }
-		        catch (SocketTimeoutException e) {
-		        }
-			}
-			catch (IOException e) {
-			}
-		}
-		return null;
-	}
-	
-	public void sendCommand(KettleCommand command) {
-		String message = "set sys output"+command.code();
-		send(message += "\n");
-	}
-	
-	public void askButtonStatus() throws IOException {
-		send("get sys status\n");
-	}
-	
-	public KettleStatus[] getStatus() {
-		List<KettleStatus> result = new ArrayList<KettleStatus>();
-		listener.transferQueue.drainTo(result);
-		return (KettleStatus[]) result.toArray(new KettleStatus[result.size()]);
-	}
-	
-	private void send(OutputStream out, String message) {
-		try {
-			out.write(message.getBytes(StandardCharsets.US_ASCII));
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void send(String message) {
-		try {
-			outputStream.write(message.getBytes(StandardCharsets.US_ASCII));
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void close() throws IOException {
-		listener.closing = true;
-		try {
-			listenerThread.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		outputStream.close();
-		inputStream.close(); 
-		sock.close();
-	}
-	
+    public Kettle() {
+        this("192.168.1.");
+    }
+    public Kettle(String networkPrefix) {
+        this.ipPrefix = networkPrefix;
+        sock = scan();
+        if(sock != null) {
+            this.hostname = sock.getInetAddress().getHostAddress();
+            try {
+                inputStream = sock.getInputStream();
+                outputStream = sock.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            listener = new KettleListener(inputStream);
+            listenerThread = new Thread(listener);
+            listenerThread.start();
+        }
+        else System.out.println("No kettle found");
+    }
+    
+    public Kettle(InetAddress address){
+        //Store hostname and port
+        this.hostname = address.getHostAddress();
+        
+        try {
+            sock = new Socket(hostname, port); //Attempt to create a socket
+            outputStream = sock.getOutputStream();
+            inputStream = sock.getInputStream();
+        }
+        catch (IOException e) {
+            System.out.println("Could not create socket");
+            e.printStackTrace();
+        }
+        listener = new KettleListener(inputStream);
+        listenerThread = new Thread(listener);
+        listenerThread.start();
+    }
+    
+    public Socket scan() {
+        for (int i = 1; i < 254; i++){
+            System.out.println(ipPrefix+i);
+            try {
+                Socket mySocket = new Socket();
+                SocketAddress address = new InetSocketAddress(ipPrefix+i, port);
+                mySocket.setSoTimeout(2000);
+                mySocket.connect(address, 500);   
+                
+                BufferedReader in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
+                send(mySocket.getOutputStream(), "HELLOKETTLE\n");
+                try {
+                    String response = in.readLine();
+                    if (response.startsWith("HELLOAPP")) {
+                        return mySocket;
+                    }
+                }
+                catch (SocketTimeoutException e) {
+                }
+            }
+            catch (IOException e) {
+            }
+        }
+        return null;
+    }
+    
+    public void sendCommand(KettleCommand command) {
+        String message = "set sys output"+command.code();
+        send(message += "\n");
+    }
+    
+    public void askButtonStatus() throws IOException {
+        send("get sys status\n");
+    }
+    
+    public KettleStatus[] getStatus() {
+        List<KettleStatus> result = new ArrayList<KettleStatus>();
+        listener.transferQueue.drainTo(result);
+        return (KettleStatus[]) result.toArray(new KettleStatus[result.size()]);
+    }
+    
+    private void send(OutputStream out, String message) {
+        try {
+            out.write(message.getBytes(StandardCharsets.US_ASCII));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void send(String message) {
+        try {
+            outputStream.write(message.getBytes(StandardCharsets.US_ASCII));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void close() throws IOException {
+        listener.closing = true;
+        try {
+            listenerThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        outputStream.close();
+        inputStream.close(); 
+        sock.close();
+    }
+    
 }
--- src/com/jacobmillward/libkettlecontrol/KettleListener.java
+++ src/com/jacobmillward/libkettlecontrol/KettleListener.java
@@ -8,109 +8,109 @@ import java.nio.charset.StandardCharsets;
 import java.util.concurrent.LinkedTransferQueue;
 
 class KettleListener implements Runnable {
-	public volatile BufferedReader inputReader;
-	public LinkedTransferQueue<KettleStatus> transferQueue;
-	public volatile boolean closing = false;
-	
-	public KettleListener(InputStream inputStream) {
-		this.inputReader = new BufferedReader(new InputStreamReader(inputStream));
-		transferQueue = new LinkedTransferQueue<KettleStatus>();
-	}
-	
-	public void run() {
-		//For lifetime of thread
-		while(!closing) {
-			String statusString;
-			try {
-				if(inputReader.ready()) {
-					statusString = inputReader.readLine();
-					for(KettleStatus status : handleMessage(statusString)) {
-						transferQueue.offer(status);
-					}
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		try {
-			inputReader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return;
-	}
-	
-	private KettleStatus[] handleMessage(String message) {
-		//Check if message is asynchronous update or requested status
-		KettleStatus[] result = null;
-		if(message.startsWith("sys status key=")) {
-			//Check if there is a status character
-			if(message.length() == 18) {
-				//Convert the 16th byte into a boolean array containing the first 6 bits
-				//16th byte of response contains information on which buttons are active
-				byte[] StatusByte = message.substring(15, 15).getBytes(StandardCharsets.US_ASCII);
-				boolean statusBoolArray[] = new boolean[6];
-			    statusBoolArray[0] = ((StatusByte[0] & 0x01) != 0);
-			    statusBoolArray[1] = ((StatusByte[0] & 0x02) != 0);
-			    statusBoolArray[2] = ((StatusByte[0] & 0x04) != 0);
-			    statusBoolArray[3] = ((StatusByte[0] & 0x08) != 0);
-			    statusBoolArray[4] = ((StatusByte[0] & 0x10) != 0);
-			    statusBoolArray[5] = ((StatusByte[0] & 0x20) != 0);
-			    
-			    int counter = 0;
-			    for(boolean value : statusBoolArray) {
-			    	if (value) counter++;
-			    }
-			    
-			    //Fill array with correct status.
-			    result = new KettleStatus[counter];
-			    int i = 0;
-			    if(statusBoolArray[0]) {
-			    	result[i] = KettleStatus.TURNED_ON; 
-			    	i++;
-			    }
-			    if(statusBoolArray[1]) {
-			    	result[i] = KettleStatus.SEL_WARM;
-			    	i++;
-			    }
-			    if(statusBoolArray[2]) {
-			    	result[i] = KettleStatus.SEL_65C;
-			    	i++;
-			    }
-				if(statusBoolArray[3]) {
-					result[i] = KettleStatus.SEL_80C;
-					i++;
-				}
-				if(statusBoolArray[4]) {
-					result[i] = KettleStatus.SEL_95C;
-					i++;
-				}
-				if(statusBoolArray[5]) {
-					result[i] = KettleStatus.SEL_100C;
-					i++;
-				}
-			}
-			//If no status character
-			result = new KettleStatus[0];
-		}
-		//If HELLOAPP response
-		else if (message.startsWith("HELLOAPP")) {
-			result = new KettleStatus[1];
-			result[0] = KettleStatus.HELLO;
-		}
-		//If asynchronous update
-		else {
-			String status = message.substring(11); //Code begins here
-			result = new KettleStatus[1]; //Prepare variable to hold single result
-			
-			for(KettleStatus kStatus : KettleStatus.values()) {
-				if (kStatus.code().equals(status)) {
-					result[0] = kStatus;
-					break;
-				}
-			}
-		}
-		
-		return result;
-	}
+    public volatile BufferedReader inputReader;
+    public LinkedTransferQueue<KettleStatus> transferQueue;
+    public volatile boolean closing = false;
+    
+    public KettleListener(InputStream inputStream) {
+        this.inputReader = new BufferedReader(new InputStreamReader(inputStream));
+        transferQueue = new LinkedTransferQueue<KettleStatus>();
+    }
+    
+    public void run() {
+        //For lifetime of thread
+        while(!closing) {
+            String statusString;
+            try {
+                if(inputReader.ready()) {
+                    statusString = inputReader.readLine();
+                    for(KettleStatus status : handleMessage(statusString)) {
+                        transferQueue.offer(status);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            inputReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return;
+    }
+    
+    private KettleStatus[] handleMessage(String message) {
+        //Check if message is asynchronous update or requested status
+        KettleStatus[] result = null;
+        if(message.startsWith("sys status key=")) {
+            //Check if there is a status character
+            if(message.length() == 18) {
+                //Convert the 16th byte into a boolean array containing the first 6 bits
+                //16th byte of response contains information on which buttons are active
+                byte[] StatusByte = message.substring(15, 15).getBytes(StandardCharsets.US_ASCII);
+                boolean statusBoolArray[] = new boolean[6];
+                statusBoolArray[0] = ((StatusByte[0] & 0x01) != 0);
+                statusBoolArray[1] = ((StatusByte[0] & 0x02) != 0);
+                statusBoolArray[2] = ((StatusByte[0] & 0x04) != 0);
+                statusBoolArray[3] = ((StatusByte[0] & 0x08) != 0);
+                statusBoolArray[4] = ((StatusByte[0] & 0x10) != 0);
+                statusBoolArray[5] = ((StatusByte[0] & 0x20) != 0);
+                
+                int counter = 0;
+                for(boolean value : statusBoolArray) {
+                    if (value) counter++;
+                }
+                
+                //Fill array with correct status.
+                result = new KettleStatus[counter];
+                int i = 0;
+                if(statusBoolArray[0]) {
+                    result[i] = KettleStatus.TURNED_ON; 
+                    i++;
+                }
+                if(statusBoolArray[1]) {
+                    result[i] = KettleStatus.SEL_WARM;
+                    i++;
+                }
+                if(statusBoolArray[2]) {
+                    result[i] = KettleStatus.SEL_65C;
+                    i++;
+                }
+                if(statusBoolArray[3]) {
+                    result[i] = KettleStatus.SEL_80C;
+                    i++;
+                }
+                if(statusBoolArray[4]) {
+                    result[i] = KettleStatus.SEL_95C;
+                    i++;
+                }
+                if(statusBoolArray[5]) {
+                    result[i] = KettleStatus.SEL_100C;
+                    i++;
+                }
+            }
+            //If no status character
+            result = new KettleStatus[0];
+        }
+        //If HELLOAPP response
+        else if (message.startsWith(KettleStatus.HELLO.code())) {
+            result = new KettleStatus[1];
+            result[0] = KettleStatus.HELLO;
+        }
+        //If asynchronous update
+        else {
+            String status = message.substring(11); //Code begins here
+            result = new KettleStatus[1]; //Prepare variable to hold single result
+            
+            for(KettleStatus kStatus : KettleStatus.values()) {
+                if (kStatus.code().equals(status)) {
+                    result[0] = kStatus;
+                    break;
+                }
+            }
+        }
+        
+        return result;
+    }
 }
